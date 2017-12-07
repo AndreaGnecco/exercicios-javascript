0327aab3a7d0b1b23496bd5cbcc55f23821cb517
Andrea Gnecco <andreagnecco@hotmail.com>
2017-12-07 17:28:56 -0200

    Mais alguns exercicios

--- main.js
+++ main.js
@@ -13,13 +13,29 @@
 
 // ----------- EXERCICIO 02 - VARIAVEIS SIMPLES -----------
 
-var nasc = prompt('Digite o ano do seu nascimento');
-var anoAtual = prompt('Digite o ano atual');
+// var nasc = prompt('Digite o ano do seu nascimento');
+// var anoAtual = prompt('Digite o ano atual');
 
-var idade = anoAtual - nasc;
-var idadeSem = (idade * ;
+// var idade = anoAtual - nasc;
+// var idadeSem = (idade * 365) / 7;
 
-document.write(idade + ' anos, ou ' + idadeSem + ' semanas. ');
+// document.write(idade + ' anos, ou ' + idadeSem + ' semanas. ');
 
 
+// ----------- EXERCICIO 03 - VARIAVEIS SIMPLES -----------
 
+// var numero = prompt('Digite um número');
+
+// document.write(numero * 2);
+
+// ----------- EXERCICIO 04 - VARIAVEIS SIMPLES COM PROPRIEDADES -----------
+
+var pessoa = {
+    nome : prompt('Digite seu nome'),
+    peso : parseFloat(prompt('Digite seu peso em kg')),
+    altura : parseFloat(prompt('Digite sua altura em centímetros'))
+};
+
+var imc = pessoa.peso / pessoa.altura * pessoa.altura;
+
+document.write(pessoa.nome + ', Seu IMC é: ' + imc);
