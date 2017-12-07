568cdc889e913d956ba8149d6e37cd4956c3f2b7
Andrea Gnecco <andreagnecco@hotmail.com>
2017-12-07 16:03:52 -0200

    Exercicios 1

---
+++ main.js
@@ -0,0 +1,25 @@
+// ----------- EXERCICIO 01 - VARIAVEIS SIMPLES -----------
+
+// var mes = prompt('Digite o mês do seu nascimento');
+// var dia = prompt('Digite o dia do seu nascimento');
+// var hora = prompt('Digite a hora do seu nascimento');
+// var minutos = prompt('Digite os minutos do seu nascimento');
+
+// var tudo = mes + ', ' + dia +  ', ' + hora + 'h e ' + minutos + 'min';
+
+// document.write(tudo);
+
+
+
+// ----------- EXERCICIO 02 - VARIAVEIS SIMPLES -----------
+
+var nasc = prompt('Digite o ano do seu nascimento');
+var anoAtual = prompt('Digite o ano atual');
+
+var idade = anoAtual - nasc;
+var idadeSem = (idade * ;
+
+document.write(idade + ' anos, ou ' + idadeSem + ' semanas. ');
+
+
+
